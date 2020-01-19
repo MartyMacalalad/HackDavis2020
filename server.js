1ce74ab453671b6c66355315384956f4fa80891f
tbaldeviso <tbbaldeviso@ucdavis.edu>
2020-01-18 22:12:18 -0800

    Removed some comments

--- server.js
+++ server.js
@@ -1,29 +1,3 @@
-/*const express=require('express');
-const path = require('path');
-const app=express();
-const vision = require('@google-cloud/vision');
-const client = new vision.ImageAnnotatorClient({
-    keyFilename: 'hackdavis2020-265600-fcccb5d30492.json'
-});
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
-    // Performs label detection on the image file
-    client
-        .labelDetection('./waterbottle.jfif')
-        .then(results => {
-            const labels = results[0].labelAnnotations;
-            res.json(labels)
-        })
-        .catch(err => {
-            console.error('ERROR:', err);
-        });
-})
-
-const port =8081;
-app.listen(port,()=>{
-console.log(`App running on ${port}`);
-})*/
 var express = require('express');
 var fs = require('fs');
 var util = require('util');
